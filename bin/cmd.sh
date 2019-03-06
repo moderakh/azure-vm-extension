0254550389abe44868a9ab76b98328d85d810866
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2019-03-06 10:25:09 -0800

    update

--- bin/cmd.sh
+++ bin/cmd.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-#start on first error
-set -o 
-
 echo "downloading upload-agent"
 wget "https://raw.githubusercontent.com/moderakh/azure-vm-extension/master/lib/upload-agent.jar" -O /tmp/upload-agent
-echo "cleaning up upload agent (if registered before"
+echo "cleaning up upload agent (if registered before)"
 sudo java -jar upload-agent.jar -cleanup
 echo "registering upload agent"
 sudo java -jar upload-agent.jar -registerAsService
