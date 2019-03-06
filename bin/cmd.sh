c00d3616b1043a5eb7fad04ccb5d8384302d26d5
Mohammad Derakhshani <moderakh@users.noreply.github.com>
2019-03-06 10:40:58 -0800

    cmd update

--- bin/cmd.sh
+++ bin/cmd.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo HELLO
+
+echo "download jar"
+wget "https://raw.githubusercontent.com/moderakh/azure-vm-extension/master/lib/upload-agent.jar" -O /tmp/upload-agent.jar
+
+echo "ceanup"
+sudo java -jar /tmp/upload-agent.jar -cleanup
+
+echo "register"
+sudo java -jar /tmp/upload-agent.jar -registerAsService
